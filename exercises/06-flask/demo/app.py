5ad29ceade7ac746537b42145b96b316040e4d16
Juan Pablo Sáenz <jpsaenzmo@gmail.com>
2026-03-23 14:42:56 +0100

    Uploaded the demo developed during the today's lesson

--- exercises/06-flask/demo/app.py
+++ exercises/06-flask/demo/app.py
@@ -4,8 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():    
-  return render_template("index.html")
+  items = ["Apple", "Banana", "Cherry"]
+  return render_template("index.html", pitems = items)
+
+@app.route("/ifexample")
+def conditional():   
+  return render_template("conditional.html", pusername = "Juan")
 
 @app.route("/about")
 def about():    
   return render_template("about.html")
+
+@app.route("/newuser")
+def register():    
+  return render_template("register.html")
